7d4b6f954a3afd397f04d2881587f20fdfb3632e
Olavo Curatola <olavocuratola@uol.com.br>
2018-05-04 06:59:26 -0300

    Mobile Project

--- CestaDeCompras/CestaDeCompras/Models/Usuario.cs
+++ CestaDeCompras/CestaDeCompras/Models/Usuario.cs
@@ -7,5 +7,19 @@ namespace CestaDeCompras.Models
 {
     public class Usuario
     {
+        private int idUsuario;
+        
+
+
+        public int IdUsuario {
+            get
+            {
+                return idUsuario;
+            }
+            set
+            {
+                idUsuario = value;
+            }
+        }
     }
 }
