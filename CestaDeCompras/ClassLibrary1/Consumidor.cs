b8d97c5766e1ab3db3d4778a85a3f6def9dd3564
Olavo Curatola <olavocuratola@uol.com.br>
2018-05-22 22:40:44 -0300

    Nova Versão

--- CestaDeCompras/ClassLibrary1/Consumidor.cs
+++ CestaDeCompras/ClassLibrary1/Consumidor.cs
@@ -2,27 +2,15 @@
 
 namespace ClassLibraryCesta
 {
-    class Consumidor:Pessoa
+    public class Consumidor : Pessoa
     { 
-        private String login;
+        private string login;
         private DateTime dataCadastro;
-        private String senha;
-        private String nivel;
+        private string senha;
+        private string nivel;
         private int IdConsumidor;
 
-        public Consumidor(int idPessoa, string nomePessoa, string sobrenomePessoa, string cpfPessoa, DateTime dataNascimento, string email, int idEndereco) : base(idPessoa, nomePessoa, sobrenomePessoa, cpfPessoa, dataNascimento, email, idEndereco)
-        {
-           
-        }
-
-        public Consumidor(string login, DateTime dataCadastro, string senha, string nivel, int idConsumidor)
-        {
-            this.login = login;
-            this.dataCadastro = dataCadastro;
-            this.senha = senha;
-            this.nivel = nivel;
-            IdConsumidor = idConsumidor;
-        }
+        
     }
 
 }
--- CestaDeCompras/ClassLibrary1/Pessoa.cs
+++ CestaDeCompras/ClassLibrary1/Pessoa.cs
@@ -5,22 +5,13 @@ namespace ClassLibraryCesta
   public abstract class Pessoa
     {
         private int IdPessoa;
-        private String nomePessoa;
-        private String sobrenomePessoa;
-        private String cpfPessoa;
+        private string nomePessoa;
+        private string sobrenomePessoa;
+        private string cpfPessoa;
         private DateTime dataNascimento;
-        private String email;
+        private string email;
         private int IdEndereco;
 
-        protected Pessoa(int idPessoa, string nomePessoa, string sobrenomePessoa, string cpfPessoa, DateTime dataNascimento, string email, int idEndereco)
-        {
-            IdPessoa = idPessoa;
-            this.nomePessoa = nomePessoa;
-            this.sobrenomePessoa = sobrenomePessoa;
-            this.cpfPessoa = cpfPessoa;
-            this.dataNascimento = dataNascimento;
-            this.email = email;
-            IdEndereco = idEndereco;
-        }
+        
     }
 }
