c9cf42f3bcd45492e8b2492f0bf763fc7bb4d836
Eduardocoutto <eduardocoouto@gmail.com>
2018-06-08 09:59:52 -0300

    Ajustes e Correções

--- CestaComprasFakeGenerator.py
+++ CestaComprasFakeGenerator.py
@@ -8,6 +8,7 @@ fake = Faker()
 fake = Factory.create('pt_BR')
 
 schema = "'public'"; #para o projeto usando entity framework utilize => dbo
+
 QTD_MARCA = 10;
 QTD_TIPO_MEDIDA = 10;
 QTD_PAIS = 11;
@@ -15,12 +16,11 @@ QTD_ESTADO = 12;
 QTD_CIDADE = 100;
 QTD_ENDERECO = 1000;
 QTD_ESTABLECIMENTO = 10;
-QTD_PESSOA = 2000;
+QTD_PESSOA = 5000;
 #QTD_CONSUMIDOR = 2000; #QUANTIDADE DE PESSOA E CONSUMIDOR DEVEM SER A MESMA
-QTD_LISTA_COMPRA = 200;
+QTD_LISTA_COMPRA = 100;
 QTD_PRODUTO = 18;
-QTD_COMPRA_REALIZADA = 20; #Valor maximo aleatorio por Pessoa
-QTD_COMPRA = 0;
+QTD_COMPRA = 30; #Valor maximo aleatorio por Pessoa
 QTD_LISTA_COMPRA_PRODUTO = 0;
 
 def main():
@@ -34,7 +34,7 @@ def gerarArquivo():
 
     arquivoSql.write("SET schema "+schema+";") #Schema padrão do postgres é Public
     arquivoSql.write(getScriptMarca())
-    arquivoSql.write(getScriptTipoMedida())
+    arquivoSql.write(getScriptMedida())
     arquivoSql.write(getScriptPais())
     arquivoSql.write(getScriptEstado())
     arquivoSql.write(getScriptCidade())
@@ -44,7 +44,7 @@ def gerarArquivo():
     arquivoSql.write(getScriptConsumidor())
     arquivoSql.write(getScriptListaCompra())
     arquivoSql.write(getScriptProduto())
-    arquivoSql.write(getScriptCompraRealizada())
+    arquivoSql.write(getScriptCompra())
 
     arquivoSql.close()
 
@@ -75,19 +75,19 @@ def getScriptMarca():
 		    """
     return sql;
 
-def getScriptTipoMedida():
+def getScriptMedida():
     sql = """
-    \nINSERT INTO TipoMedida (IdTipoMedida,TipoMedida)
-    VALUES  (1,'KG'),
-		(2,'Litro'),
-		(3,'Grama'),
-		(4,'UNIDADE'),
-		(5,'PACOTE'),
-		(6,'Seara'),
-		(7,'Watts'),
-		(8,'ML'),
-		(9,'CM'),
-		(10,'Gigas');
+    \nINSERT INTO Medida (IdMedida,Descricao,TipoMedida)
+    VALUES  (1,'KG',1),
+		(2,'Litro',2),
+		(3,'Grama',3),
+		(4,'UNIDADE',4),
+		(5,'PACOTE',5),
+		(6,'Seara',6),
+		(7,'Watts',7),
+		(8,'ML',8),
+		(9,'CM',9),
+		(10,'Gigas',10);
     """
     return sql
 
@@ -123,27 +123,30 @@ def getScriptEstado():
     return sql
 
 def getScriptCidade(qtd = QTD_CIDADE):
-    sql = " \nINSERT INTO Cidade (IdEstado, Nome) VALUES "
+    sql = " \nINSERT INTO Cidade (IdCidade,IdEstado, Nome) VALUES "
     idEstado = random.randint(1,8)
 
-    sql += " (1, 'Colina de Laranjeiras')"
+    sql += " (1,1, 'Colina de Laranjeiras')"
 
-    for i in range(0,qtd):
-        sql+= ",({0}, '{1}')".format(idEstado, fake.city())
+    for i in range(2,qtd):
+        sql+= ",({2},{0}, '{1}')".format(idEstado, fake.city(),i)
         idEstado = random.randint(1,8)
         sql += "\n"
     sql +=";"
     return sql;
 
 def getScriptEndereco(qtd = QTD_ENDERECO):
-    sql = " \nINSERT INTO Endereco (IdEndereco, Cep, logradouro, numero, complemento) VALUES  "
-    sql += "(1,'29167081','Primeiro','371','proximo a nada')"
+    sql = " \nINSERT INTO Endereco (IdEndereco, IdCidade, Cep, logradouro, numero, complemento) VALUES  "
+    sql += "(1, 1,'29167081','Primeiro','371','proximo a nada')"
+
     for i in range(2,qtd):
-        sql+= ",({0}, '{1}','{2}','{3}','{4}')".format(i,
+        idEstado = random.randint(1,QTD_CIDADE-1)
+
+        sql+= ",({0}, {5}, '{1}','{2}','{3}','{4}')".format(i,
                                                        fake.random_number(digits=8, fix_len=8),
                                                        fake.street_name(),
                                                        fake.random_int(min=1, max=9999),
-                                                       'algum complemento')
+                                                       'algum complemento',idEstado)
         sql += "\n"
     sql += ";"
     return sql
@@ -181,7 +184,7 @@ def getScriptPessoa(QTD = QTD_PESSOA):
 
         if registros > 500:
             limpaTela()
-            print('Quantidade de Compras: ', i)
+            print('Quantidade de Pessoas: ', i)
             print("Criando Pessoas...", porcentagem(i, QTD))
             registros = 0
 
@@ -190,16 +193,16 @@ def getScriptPessoa(QTD = QTD_PESSOA):
 
 def getScriptConsumidor(QTD = QTD_PESSOA):
     sql = " \nINSERT INTO Consumidor(IdConsumidor, IdPessoa, Login, senha, nivel, DataCadastro) VALUES   "
-    sql += " (1, 1, 'educouto', '123', 'Novato', '2018/02/01') "
+    sql += " (1, 1, 'educouto', '123',1, '2018/02/01') "
 
     for i in range(2, QTD):
         nome = fake.first_name()
         dataCadastro = fake.date(pattern="%Y-%m-%d", end_datetime=None)
         idEndereco = random.randint(1, 100)
-        sql += ",({0},{1}, '{2}','{3}','{4}','{5}')".format(i,i,
+        sql += ",({0},{1}, '{2}','{3}',{4},'{5}')".format(i,i,
                                                     nome,
                                                     123,
-                                                    nome+"@email.com",
+                                                    1,
                                                     dataCadastro)
         sql += "\n"
     sql += ";"
@@ -223,7 +226,7 @@ def getScriptProduto(QTD = QTD_PRODUTO):
     nomeProdutos = ['Banana', 'Maça', 'Peira','Arroz', 'Feijão','Suco de Caju', 'Coca-Cola','Milho', 'Miojo', 'Queijo',
                    'Requijão', 'Vinagre', 'Sal Grosso', 'Pão Fracês', 'Suco de Macarujá','Jiló', 'Fanta Uva']
 
-    sql = " \nINSERT INTO Produto (IdProduto, Nome, IdTipoMedida, IdMarca, unidade, valido) VALUES   "
+    sql = " \nINSERT INTO Produto (IdProduto, Nome, IdMedida, IdMarca, unidade, valido) VALUES   "
     sql += " (1, 'Biscoito Mabel', 1, 1,280,1) "
 
     for i in range(2, len(nomeProdutos)+2):
@@ -271,11 +274,12 @@ def getValorProdutos(lstIdsProdutos):
 
     return lstValorProdutos
 
-def getScriptCompraRealizada(QTD = QTD_COMPRA_REALIZADA):
+def getScriptCompra(QTD = QTD_COMPRA):
     sqlretorno = "\n"
     registros = 0
-    idCompraRealizada = 1;
     idCompra = 1;
+    idItemCompra = 1;
+    countItemCompra = 0;
 
     for IdConsumidor in range(1,QTD_PESSOA):
         #compraConsumidor = random.randint(1,QTD)
@@ -291,28 +295,30 @@ def getScriptCompraRealizada(QTD = QTD_COMPRA_REALIZADA):
 
             valorCompra = 0
 
-            sqlCompra = "INSERT INTO Compra(IdEstabelecimento ,IdProduto,IdCompraRealizada ,valido , preco) VALUES "
+            sqlCompra = "INSERT INTO ItemCompra(IdProduto,IdCompra ,valido , preco) VALUES "
             sqlValuesCompra = ""
 
             for i in range(0,len(lstIdProdutos)):
                 if(len(sqlValuesCompra) > 0):
                     sqlValuesCompra += ",\n"
-                sqlValuesCompra += "({0},{1},{2},1,{3})".format(idEstabelecimento,lstIdProdutos[i],idCompraRealizada,lstValorProdutos[i]);
+                sqlValuesCompra += "({0},{1},1,{2})".format(lstIdProdutos[i],idCompra,lstValorProdutos[i]);
                 valorCompra += lstValorProdutos[i];
-                idCompra+=1
+                countItemCompra += 1
+                idItemCompra+=1
                 registros+=1
 
             sqlCompra += sqlValuesCompra;
 
-            sqlCompraRealizada = " INSERT INTO CompraRealizada(IdCompraRealizada,IdConsumidor,DataCompra,ValorTotal) VALUES({0},{1},'{2}',{3}) ".\
-                format(idCompraRealizada,IdConsumidor,dataCompra,valorCompra)
+            sqlCompraRealizada = " INSERT INTO Compra(IdCompra,IdConsumidor,DataCompra,ValorTotal) VALUES({0},{1},'{2}',{3}) ".\
+                format(idCompra,IdConsumidor,dataCompra,valorCompra)
 
             sqlretorno += sqlCompraRealizada +";\n"+sqlCompra+";\n"
-            idCompraRealizada +=1
+            idCompra +=1
 
         if registros > 10000:
             limpaTela()
             print('Quantidade de Compras: ', idCompra)
+            print('Quantidade de Itens: ', countItemCompra)
             print("Criando Compras...", porcentagem(IdConsumidor, QTD_PESSOA))
             registros = 0
 
