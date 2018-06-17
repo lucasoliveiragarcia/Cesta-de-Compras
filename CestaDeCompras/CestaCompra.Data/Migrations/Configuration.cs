a72f52b5c3bee8ec13ba0d1b0f53294861ba0fd6
Lucas Garcia <lucasoliveiragarcia@live.com>
2018-06-17 09:21:07 -0300

    Arquivo de configuração camada Data

--- CestaDeCompras/CestaCompra.Data/Migrations/Configuration.cs
+++ CestaDeCompras/CestaCompra.Data/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace CestaCompra.Data.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(CestaCompra.Data.ContextCestaBD context)
