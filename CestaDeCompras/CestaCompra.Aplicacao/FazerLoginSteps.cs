d1a66e236f91f7571e8371a59edb6b1a8412c780
Olavo Curatola <olavocuratola@uol.com.br>
2018-06-19 11:08:08 -0300

    Inicio da Revisão  de BDD

---
+++ CestaDeCompras/CestaCompra.Aplicacao/FazerLoginSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace CestaCompra.Aplicacao
+{
+    [Binding]
+    public class FazerLoginSteps
+    {
+        [Given(@"que eu consumidor acesso a página de entrada do sistema")]
+        public void DadoQueEuConsumidorAcessoAPaginaDeEntradaDoSistema()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"eu digito o  meu (.*)")]
+        public void DadoEuDigitoOMeu(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"eu digito a  minha (.*)")]
+        public void DadoEuDigitoAMinha(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"eu errei meu (.*)")]
+        public void DadoEuErreiMeu(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"eu errei minha (.*)")]
+        public void DadoEuErreiMinha(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"eu pressiono o botão entrar")]
+        public void QuandoEuPressionoOBotaoEntrar()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"eu acesso a página do menu principal\.")]
+        public void EntaoEuAcessoAPaginaDoMenuPrincipal_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"o sistema informa falha de login dados incorretos")]
+        public void EntaoOSistemaInformaFalhaDeLoginDadosIncorretos()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ CestaDeCompras/CestaCompra.Aplicacao/Features/FazerLogin.feature
@@ -0,0 +1,20 @@
+﻿#language: pt-br
+
+Funcionalidade: FazerLogin
+	
+Cenário:Sucesso no Login
+Dado que eu consumidor acesso a página de entrada do sistema
+E eu digito o  meu <login> 
+E eu digito a  minha <senha>
+Quando  eu pressiono o botão entrar
+Então eu acesso a página do menu principal.
+
+Cenário:Insucesso no Login - Login
+Dado eu errei meu <login>
+Quando eu pressiono o botão entrar
+Então o sistema informa falha de login dados incorretos
+
+Cenário:Insucesso no Login - Senha
+Dado eu errei minha <senha>
+Quando eu pressiono o botão entrar
+Então o sistema informa falha de login dados incorretos
