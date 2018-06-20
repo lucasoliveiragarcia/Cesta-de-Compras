5999e209d00c391cf81c5809fffa5fadafb62b7a
Lucas Garcia <lucasoliveiragarcia@live.com>
2018-06-20 15:04:52 -0300

    Classes de Aplicação

---
+++ CestaDeCompras/CestaCompra.Aplicacao/CadastrarConsumidorAplicacao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CestaCompra.Aplicacao
+{
+    class CadastrarConsumidorAplicacao
+    {
+        /*public void inserirConsumidor(Consumidor consumidor)
+        {
+
+        }
+
+       public void alterarConsumidor(Consumidor consumidor)
+       {
+
+       }
+
+       public void alterarSenha(Consumidor consumidor)
+       {
+
+       }*/
+    }
+}
---
+++ CestaDeCompras/CestaCompra.Aplicacao/FazerLoginAplicacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CestaCompra.Aplicacao
+{
+    class FazerLoginAplicacao
+    {
+        /*public Consumidor verificarLogin(Consumidor consumidor)
+        {
+
+        }*/
+    }
+}
