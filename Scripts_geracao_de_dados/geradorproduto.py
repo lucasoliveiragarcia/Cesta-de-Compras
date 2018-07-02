f0ad52ce1359533cc5222000bd6b9d50a44ebe4f
lucasoliveiragarcia <lucasoliveiragarcia@live.com>
2018-07-02 08:41:31 -0300

    Update geradorproduto.py

--- Scripts_geracao_de_dados/geradorproduto.py
+++ Scripts_geracao_de_dados/geradorproduto.py
@@ -31,7 +31,7 @@ def getScriptProduto():
 	sql='INSERT INTO produto (idproduto, nome, idmedida, idmarca, unidade, valido) VALUES\n'
 	idproduto=1;
 	cont=1
-	for i in range(1,16701):
+	for i in range(1,100001):
 		nome= ['Banana', 'Maçã', 'Pera','Arroz', 'Feijão','Suco de Caju','Milho', 'Miojo', 'Queijo',
                    'Requijão', 'Vinagre', 'Sal Grosso', 'Pão Fracês', 'Suco de Macarujá','Jiló', 'Refrigerante']
 		indexNomeLista = random.randint(0, len(nome)-1)           		
@@ -39,7 +39,7 @@ def getScriptProduto():
 		preco=round(random.uniform(5, 100),2)
 		idmedida=random.randint(1, 10)
 		idmarca=random.randint(1, 10)
-		if cont!=16700:
+		if cont!=100000:
 			sql += "("+str(idproduto)+",'"+nome[indexNomeLista]+"',"+str(idmedida)+","+str(idmarca)+",1,1),\n"
 		else:
 			sql += "("+str(idproduto)+",'"+nome[indexNomeLista]+"',"+str(idmedida)+","+str(idmarca)+",1,1);"				
